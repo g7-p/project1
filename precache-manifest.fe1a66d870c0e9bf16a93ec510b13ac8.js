a2fb3ffafd5ae62d0be19edc78f30c08cfcd2db7
g7-p <spujalahari@gmail.com>
2020-03-12 15:43:14 +0530

    Updates

---
+++ precache-manifest.fe1a66d870c0e9bf16a93ec510b13ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6aa203cf6278797cb4de7acb50a144e",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "a1d2a2ddf8289f0150b5",
+    "url": "/project1/static/css/main.13eab888.chunk.css"
+  },
+  {
+    "revision": "9f94518efdbb9c5ed1eb",
+    "url": "/project1/static/js/2.f469b1c7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.f469b1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d2a2ddf8289f0150b5",
+    "url": "/project1/static/js/main.cbaf43d5.chunk.js"
+  },
+  {
+    "revision": "5e8788b335230a39461d",
+    "url": "/project1/static/js/runtime-main.1d41ff4f.js"
+  },
+  {
+    "revision": "875cdc770f0df7c5c80c0645806a467e",
+    "url": "/project1/static/media/woman.875cdc77.svg"
+  }
+]);
